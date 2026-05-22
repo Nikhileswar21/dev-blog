866d66b806d42de87c31cf1ab7fb8e831d7901b7
nikhil <nikhileswarsivakumar@gmail.com>
2026-05-22 15:10:36 +0530

    ejs and bootstrap included

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 // import the routes
@@ -8,13 +9,21 @@ const blogRoute = require('./routes/blogRoute');
 const staticRoute = require('./routes/staticRoute');
 
 
+//configurations
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+
+
 //middleware
+app.use(express.static(path.resolve('./public')));
 app.use(bodyParser.json());
 
 
+
 // register the routes 
+app.use('/', staticRoute);
 app.use('/user', userRoute);
 app.use('/blog', blogRoute);
-app.use('/static', staticRoute);
+
 
 app.listen(8000, () => console.log('server started at port 8000'));
--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/', function(req, res){
-    res.end('route under construction');
+    res.render('home');
 });
 
 
