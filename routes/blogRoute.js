0bdaa2ea03874e35e2431f696149bdc86c3af179
nikhil <nikhileswarsivakumar@gmail.com>
2026-05-28 19:32:29 +0530

    blog form

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', function(req, res){
-    res.end('route under construction');
+router.get('/create', function(req, res){
+    res.render('createblog');
 });
 
 
