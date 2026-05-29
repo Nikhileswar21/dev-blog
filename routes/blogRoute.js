1416b55563a18bcf618c6641654a3d64e02739cb
nikhil <nikhileswarsivakumar@gmail.com>
2026-05-29 12:58:21 +0530

    file upload storing using the multer

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const multer = require('multer');
+
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination : (req,file,cb) =>{
+        cb(null, ('./public/uploads/'));
+    },
+    filename : (req,file,cb) => {
+    cb(null, file.originalname);
+}
+});
+
+const upload = multer({storage});
 
 
 router.get('/create', function(req, res){
     res.render('createblog');
 });
 
-router.post('/create', function(req,res){
+router.post('/create', upload.single('coverImg'), function(req,res){
     console.log(req.body);
+    console.log(req.file);
     return res.render('createblog');
 })
 
